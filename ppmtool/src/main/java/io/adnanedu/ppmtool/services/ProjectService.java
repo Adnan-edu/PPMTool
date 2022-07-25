f44afd6e22eaf0c9745ada6a35d88acdb24c1c6c
mdashikadnan <md.ashikadnan@gmail.com>
2022-07-25 13:12:00 +1000

    Custom exception for unique project identifier

--- ppmtool/src/main/java/io/adnanedu/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/adnanedu/ppmtool/services/ProjectService.java
@@ -2,6 +2,7 @@ package io.adnanedu.ppmtool.services;
 
 import io.adnanedu.ppmtool.domain.Project;
 import io.adnanedu.ppmtool.dto.ProjectDto;
+import io.adnanedu.ppmtool.exceptions.ProjectIdException;
 import io.adnanedu.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,11 @@ public class ProjectService {
     public Project saveOrUpdateProject(ProjectDto projectDto){
         Project project = new Project();
         BeanUtils.copyProperties(projectDto, project);
-        //Logic
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
