ff078511927f67b3f65468b9e6ee524cd5dbddce
mdashikadnan <md.ashikadnan@gmail.com>
2022-07-25 11:44:40 +1000

    project object validation

--- ppmtool/src/main/java/io/adnanedu/ppmtool/dto/ProjectDto.java
+++ ppmtool/src/main/java/io/adnanedu/ppmtool/dto/ProjectDto.java
@@ -1,14 +1,25 @@
 package io.adnanedu.ppmtool.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 public class ProjectDto {
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message ="Project Identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 }
--- ppmtool/src/main/java/io/adnanedu/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/adnanedu/ppmtool/web/ProjectController.java
@@ -2,14 +2,22 @@ package io.adnanedu.ppmtool.web;
 
 import io.adnanedu.ppmtool.domain.Project;
 import io.adnanedu.ppmtool.dto.ProjectDto;
+import io.adnanedu.ppmtool.services.MapValidationErrorService;
 import io.adnanedu.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,9 +26,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    MapValidationErrorService mapValidationErrorService;
+
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody ProjectDto projectDto){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody ProjectDto projectDto, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null)
+            return errorMap;
         Project project = projectService.saveOrUpdateProject(projectDto);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
